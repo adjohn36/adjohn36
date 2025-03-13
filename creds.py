b8597623c0de9ecd8439124b7659a7e718f8f376
adjohn36 <132848782+adjohn36@users.noreply.github.com>
2025-03-13 02:48:28 -0400

    Delete creds.py

--- creds.py
+++
@@ -1,5 +0,0 @@
-sender = "adampricescraper@gmail.com"
-recipient = "adamj00001@gmail.com"
-password = input("Please enter your password: ")
-
-#token = "mlsn.df87ef9fcf9e01d965eb53b9a646f2803d00260b400cfb0f1c0d362920196ad0"
